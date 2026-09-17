74679ca437fe51ea856b3513f151752da81c6276
Juan Carlos <juan_carlos_medrano@hotmail.com>
2026-09-17 16:41:46 -0400

    feat: configure OpenIddict authorization server and OAuth client

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
 using AuthCore.Data;
 using Microsoft.EntityFrameworkCore;
+using OpenIddict.Abstractions;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+string clientId = builder.Configuration["OAuth:ClientId"]
+    ?? throw new InvalidOperationException(
+       "OAuth:ClientId is not configured.");
+
+string clientSecret = builder.Configuration["OAuth:ClientSecret"]
+    ?? throw new InvalidOperationException(
+        "OAuth:ClientSecret is not configured.");
 
 // Add services to the container.
 
@@ -25,14 +34,35 @@ builder.Services.AddOpenIddict()
     .AddServer(options =>
     {
         options.SetTokenEndpointUris("/connect/token");
-
         options.AllowClientCredentialsFlow();
-
         options.AddDevelopmentEncryptionCertificate();
         options.AddDevelopmentSigningCertificate();
+
+        options.UseAspNetCore();
+
     });
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var applicationManager = scope.ServiceProvider.GetRequiredService<OpenIddict.Abstractions.IOpenIddictApplicationManager>();
+
+    var existingClient = await applicationManager.FindByClientIdAsync(clientId);
+
+    if (existingClient is null)
+    {
+        var descriptor = new OpenIddictApplicationDescriptor
+        {
+            ClientId = clientId,
+            ClientSecret = clientSecret,
+            ClientType = OpenIddictConstants.ClientTypes.Confidential
+        };
+
+
+        await applicationManager.CreateAsync(descriptor);
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
